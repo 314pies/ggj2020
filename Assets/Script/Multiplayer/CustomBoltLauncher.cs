5e27ef94f642888912572851a5c6ea880bed6bfb
314pie <ian121363@gmail.com>
2020-02-01 19:05:11 +0800

    Add player spawning and position syncing

---
+++ Assets/Script/Multiplayer/CustomBoltLauncher.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Bolt.Matchmaking;
+using UdpKit;
+using UnityEngine;
+
+public class CustomBoltLauncher : Bolt.GlobalEventListener
+{
+	void OnGUI()
+	{
+        if (BoltNetwork.IsRunning) { return; }
+		GUILayout.BeginArea(new Rect(10, 10, Screen.width - 20, Screen.height - 20));
+
+		if (GUILayout.Button("Start Server", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
+		{
+			// START SERVER
+			BoltLauncher.StartServer();
+		}
+
+		if (GUILayout.Button("Start Client", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
+		{
+			// START CLIENT
+			BoltLauncher.StartClient();
+		}
+
+		GUILayout.EndArea();
+	}
+
+	public override void BoltStartDone()
+	{
+		if (BoltNetwork.IsServer)
+		{
+			string matchName = Guid.NewGuid().ToString();
+
+			BoltMatchmaking.CreateSession(
+				sessionID: matchName
+			);
+
+            var _player = BoltNetwork.Instantiate(BoltPrefabs.Player);
+            _player.TakeControl();
+
+        }
+	}
+
+
+    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+    {
+        Debug.LogFormat("Session list updated: {0} total sessions", sessionList.Count);
+
+        foreach (var session in sessionList)
+        {
+            UdpSession photonSession = session.Value as UdpSession;
+
+            if (photonSession.Source == UdpSessionSource.Photon)
+            {
+                BoltNetwork.Connect(photonSession);
+            }
+        }
+    }
+
+    public override void Connected(BoltConnection connection)
+    {
+        if (BoltNetwork.IsClient)
+        {
+            var _player = BoltNetwork.Instantiate(BoltPrefabs.Player);
+            _player.TakeControl();
+        }
+    }
+}
---
+++ Assets/Script/Multiplayer/PlayerAgent.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+using CliffLeeCL;
+public class PlayerAgent : EntityBehaviour<IPlayerState>
+{
+    public override void Attached()
+    {
+        state.SetTransforms(state.Position, transform);
+        state.SetAnimator(GetComponent<Animator>());
+        GetComponent<PlayerInteraction>().enabled = false;
+        GetComponent<PlayerController2D>().enabled = false;
+        GetComponent<PlayerAnimator>().enabled = false;
+        state.AddCallback("localScaleX", ()=> {
+            transform.localScale = new Vector3(state.localScaleX, transform.localScale.y, transform.localScale.z);
+        });
+
+        //GetComponent<Rigidbody2D>().isKinematic = true;
+    }
+
+    private void Update()
+    {
+        if (entity.IsOwner)
+        {
+            state.localScaleX = transform.localScale.x;
+        }
+    }
+
+    public override void ControlGained()
+    {
+        GetComponent<PlayerInteraction>().enabled = true;
+        GetComponent<PlayerController2D>().enabled = true;
+        GetComponent<PlayerAnimator>().enabled = true;
+        //GetComponent<Rigidbody2D>().isKinematic = false;
+    }
+
+    public override void ControlLost()
+    {
+        GetComponent<PlayerInteraction>().enabled = false;
+        GetComponent<PlayerController2D>().enabled = false;
+        GetComponent<PlayerAnimator>().enabled = false;
+
+        //GetComponent<Rigidbody2D>().isKinematic = true;
+    }
+
+    //public override void SimulateController()
+    //{
+    //    ICustomPlayerCommandInput input = CustomPlayerCommand.Create();
+
+    //    input.Position = transform.position;
+    //    input.Velocity = GetComponent<Rigidbody2D>().velocity;
+
+    //    entity.QueueInput(input);
+    //}
+
+    //public override void ExecuteCommand(Command command, bool resetState)
+    //{
+    //    CustomPlayerCommand cmd = (CustomPlayerCommand)command;
+
+    //    if (resetState)
+    //    {
+    //        // we got a correction from the server, reset (this only runs on the client)
+    //       // _motor.SetState(cmd.Result.Position, cmd.Result.Velocity, cmd.Result.IsGrounded, cmd.Result.JumpFrames);
+    //    }
+    //    else//Execute on server and controller
+    //    {
+    //        if (entity.IsOwner)
+    //        {
+    //            transform.position = cmd.Input.Position;
+    //            GetComponent<Rigidbody2D>().velocity = cmd.Input.Velocity;
+    //        }
+    //        cmd.Result.Position = transform.position;
+    //    }
+    //}
+}
