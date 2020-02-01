858711a2f4f01dc038af174018412895ef134bee
Cliff Lee CL <tommyboys0107@gmail.com>
2020-02-01 16:01:59 +0800

    [Import] Import CliffLeeCL's old scripts.

---
+++ Assets/CliffLeeCL/Script/UI/InGameMenu.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CliffLeeCL
+{
+    /// <summary>
+    /// The class controls how in-game menu works.
+    /// </summary>
+    public class InGameMenu : MonoBehaviour
+    {
+        /// <summary>
+        /// Objects that should be set active when UI is opened.
+        /// </summary>
+        public GameObject[] objectsToOpen;
+        /// <summary>
+        /// Objects that should be set inactive when UI is opened.
+        /// </summary>
+        public GameObject[] objectsToClose;
+
+        /// <summary>
+        /// Is true when the game is over.
+        /// </summary>
+        bool isGameOver = false;
+
+        /// <summary>
+        /// Start is called once on the frame when a script is enabled.
+        /// </summary>
+        void Start()
+        {
+            EventManager.Instance.onGameOver += OnGameOver;
+        }
+
+        /// <summary>
+        /// Update is called every frame, if the MonoBehaviour is enabled.
+        /// </summary>
+        void Update()
+        {
+            if (!isGameOver && Input.GetKeyDown(KeyCode.Escape))
+            {
+                TogglePause();
+            }
+        }
+
+        /// <summary>
+        /// This function is called when the behaviour becomes disabled () or inactive.
+        /// </summary>
+        void OnDisable()
+        {
+            EventManager.Instance.onGameOver -= OnGameOver;
+        }
+
+        /// <summary>
+        /// Event listener that listen to EventManager's onGameOver event.
+        /// </summary>
+        /// <seealso cref="EventManager.onGameOver"/>
+        void OnGameOver()
+        {
+            isGameOver = true;
+        }
+
+        /// <summary>
+        /// Used to toggle pause.
+        /// </summary>
+        void TogglePause()
+        {
+            if (Time.timeScale > 0.0f)
+            {
+                Time.timeScale = 0.0f;
+                ToggleUI(true);
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Time.timeScale = 1.0f;
+                ToggleUI(false);
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+
+        /// <summary>
+        /// Used to toggle UI.
+        /// </summary>
+        /// <param name="isActive"></param>
+        void ToggleUI(bool isActive)
+        {
+            foreach (GameObject obj in objectsToOpen)
+                obj.SetActive(isActive);
+            foreach (GameObject obj in objectsToClose)
+                obj.SetActive(!isActive);
+        }
+
+        /// <summary>
+        /// Is called when resume button is called. Resume the game.
+        /// </summary>
+        public void ResumeButtonCallback()
+        {
+            TogglePause();
+        }
+    }
+}
---
+++ Assets/CliffLeeCL/Script/UI/Rank.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CliffLeeCL {
+    public class Rank : MonoBehaviour {
+        public Text rankText;
+        public int maxScore = 1000000;
+
+        // Use this for initialization
+        void Start() {
+            EventManager.Instance.onGameOver += OnGameOver;
+        }
+
+        void OnDisable()
+        {
+            EventManager.Instance.onGameOver -= OnGameOver;
+        }
+
+        void OnGameOver()
+        {
+            int currentScore = ScoreManager.Instance.CurrentScore;
+
+            if(currentScore > maxScore * 0.8f)
+            {
+                rankText.text = "S";
+            }
+            else if (currentScore > maxScore * 0.5f)
+            {
+                rankText.text = "A";
+            }
+            else if (currentScore > maxScore * 0.2f)
+            {
+                rankText.text = "B";
+            }
+            else
+            {
+                rankText.text = "C";
+            }
+        }
+    }
+}
