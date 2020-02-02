eba9bc0fe9f1cdc3dc7d5a0f1222b42be9ff9597
314pie <ian121363@gmail.com>
2020-02-02 15:37:58 +0800

    Update My Bolt Launcher

--- Assets/Script/Multiplayer/CustomBoltLauncher.cs
+++ Assets/Script/Multiplayer/CustomBoltLauncher.cs
@@ -7,26 +7,38 @@ using UnityEngine;
 using CliffLeeCL;
 public class CustomBoltLauncher : Bolt.GlobalEventListener
 {
+    public GameObject UI;
     public Vector3 PlayerSpawnPoint;
-	void OnGUI()
-	{
-        if (BoltNetwork.IsRunning) { return; }
-		GUILayout.BeginArea(new Rect(10, 10, Screen.width - 20, Screen.height - 20));
+	//void OnGUI()
+	//{
+ //       if (BoltNetwork.IsRunning) { return; }
+	//	GUILayout.BeginArea(new Rect(10, 10, Screen.width - 20, Screen.height - 20));
 
-		if (GUILayout.Button("Start Server", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
-		{
-			// START SERVER
-			BoltLauncher.StartServer();
-		}
+	//	if (GUILayout.Button("Start Server", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
+	//	{
+	//		// START SERVER
+	//		BoltLauncher.StartServer();
+	//	}
 
-		if (GUILayout.Button("Start Client", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
-		{
-			// START CLIENT
-			BoltLauncher.StartClient();
-		}
+	//	if (GUILayout.Button("Start Client", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
+	//	{
+	//		// START CLIENT
+	//		BoltLauncher.StartClient();
+	//	}
 
-		GUILayout.EndArea();
-	}
+	//	GUILayout.EndArea();
+	//}
+
+
+    public void StartServer()
+    {
+        BoltLauncher.StartServer();
+    }
+
+    public void StartClient()
+    {
+        BoltLauncher.StartClient();
+    }
 
 	public override void BoltStartDone()
 	{
@@ -42,6 +54,7 @@ public class CustomBoltLauncher : Bolt.GlobalEventListener
             _player.TakeControl();
             SpawnSoldiers();
         }
+        UI.SetActive(false);
 	}
 
 
