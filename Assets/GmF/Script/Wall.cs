158eb3f5aee694efc36f40ba2b94af49f21958aa
gmfnasg <gpx_mark_farng@hotamil.com>
2020-02-02 14:21:26 +0800

    增加判定輸贏牆壁

---
+++ Assets/GmF/Script/Wall.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    bool isFinish = false;
+    SideEnem side = SideEnem.None;
+    public CliffLeeCL.GameOverMenu _GameOverMenu = null;
+
+    private void Awake()
+    {
+        side = gameObject.GetComponent<Side>().side;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        CheckFinish(collision.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        CheckFinish(collision.gameObject);
+    }
+
+    private void CheckFinish(GameObject hitGameObject)
+    {
+        if (isFinish)
+        {
+            return;
+        }
+
+        if (hitGameObject == null)
+        {
+            return;
+        }
+
+        Unit unit = hitGameObject.GetComponent<Unit>();
+        if (unit == null)
+        {
+            return;
+        }
+
+        if (unit.UnitType != UnitTypeEnum.Soldier)
+        {
+            return;
+        }
+
+        Side hitSide = hitGameObject.GetComponent<Side>();
+
+        if (hitSide == null)
+        {
+            return;
+        }
+
+        if (side == hitSide.side)
+        {
+            return;
+        }
+
+        _GameOverMenu.OnGameOver(side != SideEnem.Left);
+    }
+}
