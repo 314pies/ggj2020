e0d60f0483bc73e7ef77057b806565b4b3366400
ertty446688 <ertty446688@gmail.com>
2020-02-02 12:51:29 +0800

    更新掉落圖片

--- Assets/GmF/Script/ItemBase.cs
+++ Assets/GmF/Script/ItemBase.cs
@@ -4,5 +4,22 @@ using System.Collections.Generic;
 using UnityEngine;
 public class ItemBase : MonoBehaviour
 {
+    public Sprite Weapons;
+    public Sprite Equip;
     public ItemSetting itemSetting = new ItemSetting();
+    void Start()
+    {
+        SpriteRenderer mySpr;
+        mySpr =transform.Find("Sprite").gameObject.GetComponent<SpriteRenderer>();
+        if (itemSetting.ItemType == ItemTypeEnum.Weapon)
+        {
+            mySpr.sprite = Weapons;
+        }
+        else
+        {
+            mySpr.sprite = Equip;
+        }
+        
+    }
+
 }
